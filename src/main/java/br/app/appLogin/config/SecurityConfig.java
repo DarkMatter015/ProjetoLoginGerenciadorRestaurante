f7706e45f8a28df3da014cf8836d455550c81f37
DarkMatter015 <decamargoluk@gmail.com>
2025-05-22 13:41:18 -0300

    style: Estilizando páginas html, css e bootstrap e validação

--- src/main/java/br/app/appLogin/config/SecurityConfig.java
+++ src/main/java/br/app/appLogin/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login", "/cadastroUsuario", "/css/**").permitAll()
+                        .requestMatchers("/login", "/cadastroUsuario", "/css/**").permitAll()
                         .requestMatchers("/listarUsuarios", "/editar/**", "/excluir/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
